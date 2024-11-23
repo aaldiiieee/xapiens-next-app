476de8959ab328c778e7e2afc75568d6fcad9595
aaldiiieee <prmdy02@gmail.com>
2024-11-23 17:51:09 +0700

    adjust middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,8 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token");
 
-  const protectedRoutes = ["/:path*"];
-
-  if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
-    if (!token) {
-      return NextResponse.redirect(new URL("/auth/login", req.url));
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
   return NextResponse.next();
