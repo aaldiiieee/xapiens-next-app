d5b0834e6a7a9e3b32e7988f93e10f597416b0be
aaldiiieee <prmdy02@gmail.com>
2024-11-24 11:08:50 +0700

    minor change

--- src/app/utils/authOptions.ts
+++ src/app/utils/authOptions.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { JWT } from "next-auth/jwt";
