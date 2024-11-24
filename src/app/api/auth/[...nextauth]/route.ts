dfc02db804e43414f4975e1b93b189e5dc18c71f
aaldiiieee <prmdy02@gmail.com>
2024-11-24 11:06:15 +0700

    handle next auth options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,60 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { JWT } from "next-auth/jwt";
-import { Session } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import axios from "axios";
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { JWT } from "next-auth/jwt";
+// import { Session } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import axios from "axios";
 
-export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "Email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          return null;
-        }
-        const res = await axios.post("https://reqres.in/api/login", {
-          email: credentials?.email,
-          password: credentials?.password,
-        });
-        const user = await res.data;
-        if (user.token) {
-          return user;
-        } else {
-          return null;
-        }
-      }
-    })
-  ],
-  callbacks: {
-    async session({ session, token }: { session: Session; token: JWT }) {
-      (session.user as any).accessToken = token.accessToken;
-      return session;
-    },
-    async jwt({ token, account }: any) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-  },
-};
+// export const authOptions = {
+//   secret: process.env.NEXTAUTH_SECRET,
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID || "",
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+//     }),
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: { label: "Email", type: "email", placeholder: "Email" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         if (!credentials?.email || !credentials?.password) {
+//           return null;
+//         }
+//         const res = await axios.post("https://reqres.in/api/login", {
+//           email: credentials?.email,
+//           password: credentials?.password,
+//         });
+//         const user = await res.data;
+//         if (user.token) {
+//           return user;
+//         } else {
+//           return null;
+//         }
+//       }
+//     })
+//   ],
+//   callbacks: {
+//     async session({ session, token }: { session: Session; token: JWT }) {
+//       (session.user as any).accessToken = token.accessToken;
+//       return session;
+//     },
+//     async jwt({ token, account }: any) {
+//       if (account) {
+//         token.accessToken = account.access_token;
+//       }
+//       return token;
+//     },
+//   },
+// };
+
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST };
+
+import { authOptions } from "@/app/utils/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
